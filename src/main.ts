f246a970ad7a5baced23051b524a9a0e4088a8ea
TienAnh-Luu <81581431+TienAnh-Luu@users.noreply.github.com>
2024-06-05 14:00:35 +0700

    Add cors

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    app.enableCors();
-    app.use(cookieParser());
-    await app.listen(process.env.PORT || 8000);
+  const app = await NestFactory.create(AppModule);
+  // app.enableCors();
+  // Enable CORS with specific settings
+  app.enableCors({
+    origin: 'http://localhost:3000', // Allow requests from this origin
+    credentials: true, // Allow credentials (cookies, headers)
+  });
+  app.use(cookieParser());
+  await app.listen(process.env.PORT || 8000);
 }
 bootstrap();

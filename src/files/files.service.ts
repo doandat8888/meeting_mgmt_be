c7c075b00495777616d6db46d998cb0231690237
TienAnh-Luu <81581431+TienAnh-Luu@users.noreply.github.com>
2024-06-23 21:15:19 +0700

    Add logger to all modules but usermeeting

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -4,69 +4,109 @@ import { File } from './file.entity';
 import { Repository } from 'typeorm';
 import { UpdateFileDto } from './dtos/update-file.dto';
 import { UsersService } from 'src/users/users.service';
+import { CustomLoggerService } from 'src/logger/logger.service';
 
 @Injectable()
 export class FilesService {
+  constructor(
+    @InjectRepository(File) private repo: Repository<File>,
+    private userService: UsersService,
+    private readonly logger: CustomLoggerService,
+  ) {}
 
-    constructor(
-        @InjectRepository(File) private repo: Repository<File>,
-        private userService: UsersService
-    ) { }
+  async getFiles(meetingId: string): Promise<any> {
+    this.logger.log(`Fetching files for meeting ${meetingId}`);
+    const files = await this.repo.find({ where: { meetingId } });
 
-    async getFiles(meetingId: string): Promise<any> {
-        const files = await this.repo.find({ where: { meetingId } });
-        const filesWithUsers = await Promise.all(files.map(async (file) => {
-            const user = await this.userService.findOneById(file.createdBy);
-            return user ? {
-                ...file,
-                userCreateName: user.fullName
-            } : file;
-        }));
-        return filesWithUsers;
+    const filesWithUsers = await Promise.all(
+      files.map(async (file) => {
+        const user = await this.userService.findOneById(file.createdBy);
+        return user
+          ? {
+              ...file,
+              userCreateName: user.fullName,
+            }
+          : file;
+      }),
+    );
+
+    return filesWithUsers;
+  }
+
+  async delete(fileId: string) {
+    this.logger.log(`Deleting file ${fileId}`);
+    const file = await this.repo.findOne({ where: { id: fileId } });
+    return this.repo.remove(file);
+  }
+
+  async create(
+    name: string,
+    type: string,
+    link: string,
+    publicId: string,
+    meetingId: string,
+    idUser: string,
+  ): Promise<File> {
+    this.logger.log(
+      `User ${idUser} starting create file named ${name} of type ${type} with public id ${publicId} in meeting ${meetingId}`,
+    );
+
+    try {
+      let file = await this.repo.create({
+        name,
+        type,
+        link,
+        publicId,
+        meetingId,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        createdBy: idUser,
+        updatedBy: idUser,
+      });
+
+      return this.repo.save(file);
+    } catch (error) {
+      this.logger.error(
+        `Error when user ${idUser} creating file with public id ${publicId} in meeting ${meetingId}: ${error.message}`,
+        error.stack,
+      );
+      throw new BadRequestException('Internal server error');
     }
+  }
 
-    async delete(fileId: string) {
-        const file = await this.repo.findOne({ where: { id: fileId } });
-        return this.repo.remove(file);
-    }
+  async update(
+    file: File,
+    @Body() updateFileDto: UpdateFileDto,
+    userId: string,
+  ) {
+    this.logger.log(`User ${userId} is updating file ${file?.id}`);
 
-    async create(name: string, type: string, link: string, publicId: string, meetingId: string, idUser: string): Promise<File> {
-        try {
-            let file = await this.repo.create({
-                name,
-                type,
-                link,
-                publicId,
-                meetingId,
-                createdAt: new Date(),
-                updatedAt: new Date(),
-                createdBy: idUser,
-                updatedBy: idUser
-            });
-            return this.repo.save(file);
-        } catch (error) {
-            console.log(error);
-            throw new BadRequestException("Internal server error");
+    try {
+      Object.keys(updateFileDto).forEach((key) => {
+        if (
+          file[key] !== undefined &&
+          key !== 'id' &&
+          key !== 'createdAt' &&
+          key !== 'updatedAt'
+        ) {
+          file[key] = updateFileDto[key];
         }
-    }
+      });
 
-    async update(file: File, @Body() updateFileDto: UpdateFileDto, userId: string) {
-        try {
-            Object.keys(updateFileDto).forEach(key => {
-                if (file[key] !== undefined && key !== 'id' && key !== 'createdAt' && key !== 'updatedAt') {
-                    file[key] = updateFileDto[key];
-                }
-            });
+      file['updatedBy'] = userId;
 
-            file['updatedBy'] = userId;
-            return await this.repo.save(file);
-        } catch (error) {
-            console.log(error);
-            throw new BadRequestException("Internal server error");
-        }
+      return await this.repo.save(file);
+    } catch (error) {
+      this.logger.error(
+        `Error when user ${userId} updating file: ${error.message}`,
+        error.stack,
+      );
+      throw new BadRequestException('Internal server error');
     }
+  }
 
-    async findOne(fileId: string): Promise<File> {
-        return this.repo.findOne({ where: { id: fileId } });
-    }
+  async findOne(fileId: string): Promise<File> {
+    this.logger.log(`Fetching file with ID: ${fileId}`);
+    return this.repo.findOne({ where: { id: fileId } });
+  }
 }
--- src/guards/admin.guard.ts
+++ src/guards/admin.guard.ts
@@ -1,13 +1,25 @@
-import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
-import { role } from "src/users/enums/role.enum";
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CustomLoggerService } from 'src/logger/logger.service';
+import { role } from 'src/users/enums/role.enum';
 
 @Injectable()
-
 export class AdminGuard implements CanActivate {
-    canActivate(context: ExecutionContext): boolean {
-        const request = context.switchToHttp().getRequest();
-        if(!request.user) return false;
-        const user = request.user;
-        return user.role === role.admin;
+  constructor(private readonly logger: CustomLoggerService) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    if (!request.user) {
+      this.logger.warn('AdminGuard: No user found in request');
+      return false;
     }
+
+    const user = request.user;
+    const isAdmin = user.role === role.admin;
+    if (!isAdmin) {
+      this.logger.warn(`AdminGuard: User ${user.email} is not an admin`);
+    } else {
+      this.logger.log(`AdminGuard: User ${user.email} granted admin access`);
+    }
+    return isAdmin;
+  }
 }

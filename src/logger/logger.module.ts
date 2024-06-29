d60957082bc6c0f7671392a7a7c526b02206e9d3
unknown <badatdoan@gmail.com>
2024-06-29 14:18:55 +0700

    [LoggingInterCeptor]: Move logging.interceptor file to interceptors folder

--- src/logger/logger.module.ts
+++ src/logger/logger.module.ts
@@ -1,7 +1,7 @@
 import { Global, Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { CustomLoggerService } from './logger.service';
-import { LoggingInterceptor } from './logging.interceptor';
+import { LoggingInterceptor } from '../interceptors/logging.interceptor';
 
 @Global()
 @Module({
